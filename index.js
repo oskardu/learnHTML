328124969903fdc1f0d7593a2f58b54196f88dcb
oskardu <sdsbgt@gmail.com>
2018-11-12 15:59:12 +0800

    footer

---
+++ index.js
@@ -0,0 +1,90 @@
+function mouseOver()
+{
+document.getElementById("spannew").style.background="rgb(0, 153, 255)";
+}
+
+function mouseOut()
+{
+    document.getElementById('spannew').style.background = "rgba(255, 255, 255, .1)";
+}
+
+// footerTwitter那一行鼠标悬浮时效果
+function mouseOver1()
+{
+document.getElementById("img1").src="images/footer_twitter_hover.png";
+}
+
+function mouseOut1()
+{
+document.getElementById("img1").src="images/footer_twitter.png";
+}
+
+
+function mouseOver2()
+{
+document.getElementById("img2").src="images/footer_facebook_hover.png";
+}
+
+function mouseOut2()
+{
+document.getElementById("img2").src="images/footer_facebook.png";
+}
+
+
+
+function mouseOver3()
+{
+document.getElementById("img3").src="images/footer_dribbble_hover.png";
+}
+
+function mouseOut3()
+{
+document.getElementById("img3").src="images/footer_dribbble.png";
+}
+
+
+function mouseOver4()
+{
+document.getElementById("img4").src="images/footer_medium_hover.png";
+}
+
+function mouseOut4()
+{
+document.getElementById("img4").src="images/footer_medium.png";
+}
+
+
+function mouseOver5()
+{
+document.getElementById("img5").src="images/footer_youtube_hover.png";
+}
+
+function mouseOut5()
+{
+document.getElementById("img5").src="images/footer_youtube.png";
+}
+
+
+function mouseOver6()
+{
+document.getElementById("img6").src="images/footer_slack_hover.png";
+}
+
+function mouseOut6()
+{
+document.getElementById("img6").src="images/footer_slack.png";
+}
+
+
+
+// Cookie Settings 悬浮
+function mouseOver7(){
+    document.getElementById("CookieSettings").style.color="rgb(0, 153, 255)";
+    document.getElementById("CookieSettings").style.textDecoration="none";
+
+}
+
+function mouseOut7(){
+    document.getElementById("CookieSettings").style.color="#444";
+
+}
